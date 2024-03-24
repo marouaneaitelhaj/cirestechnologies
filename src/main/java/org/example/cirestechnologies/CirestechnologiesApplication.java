7efdd84c459feed5aa88a060c7d4d957f9a8dc04
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-03-24 23:25:54 +0000

    Add initial project files

---
+++ src/main/java/org/example/cirestechnologies/CirestechnologiesApplication.java
@@ -0,0 +1,13 @@
+package org.example.cirestechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirestechnologiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CirestechnologiesApplication.class, args);
+    }
+
+}
